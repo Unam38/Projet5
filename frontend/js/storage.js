f7c5a4bfd57812725a02026b00e52ce2c79034b1
Emmanuel Daubeuf <emmanueldaubeuf@gmail.com>
2020-09-14 10:26:48 +0200

    opti storage.js

--- frontend/js/storage.js
+++ frontend/js/storage.js
@@ -1,18 +1,14 @@
 const Storage = {
     engine: localStorage,
-
     has(name) {
         return this.engine.getItem(name);
     },
-    
     get(name) {
         return JSON.parse(this.engine.getItem(name));
     },
-    
     store (key, value) {
         this.engine.setItem(key, JSON.stringify(value));
     },
-    
     clear(){
         this.engine.clear();
     }
